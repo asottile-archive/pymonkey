2f299b51b255a1c452f858bf514fcf7510cb579a
Anthony Sottile <asottile@umich.edu>
2016-05-09 17:49:56 -0700

    v0.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pymonkey',
     description='A tool for applying monkeypatches to python executables.',
     url='https://github.com/asottile/pymonkey',
-    version='0.2.0',
+    version='0.2.1',
     author='Anthony Sottile',
     author_email='asottile@umich.edu',
     classifiers=[
