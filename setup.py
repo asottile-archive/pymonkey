e5d928b229fccf16807ad707163994e0dc88d3f3
Anthony Sottile <asottile@umich.edu>
2019-02-27 22:10:45 -0800

    Migrate setup.py to setup.cfg declarative metadata
    Committed via https://github.com/asottile/all-repos

--- setup.py
+++ setup.py
@@ -1,22 +1,2 @@
 from setuptools import setup
-
-setup(
-    name='pymonkey',
-    description='A tool for applying monkeypatches to python executables.',
-    url='https://github.com/asottile/pymonkey',
-    version='0.2.2',
-    author='Anthony Sottile',
-    author_email='asottile@umich.edu',
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ],
-    py_modules=['pymonkey'],
-    entry_points={'console_scripts': ['pymonkey = pymonkey:main']},
-)
+setup()
